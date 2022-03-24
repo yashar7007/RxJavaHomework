b505b39d48b538b1b2a029be9aa08bdee750b960
Yashar Nuruev <n.yashar@mail.ru>
2022-03-24 18:17:42 +0300

    First homework

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,153 @@
+import io.reactivex.rxjava3.annotations.Nullable
+import io.reactivex.rxjava3.core.*
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.functions.Action
+import jdk.jfr.Timespan
+import org.jetbrains.annotations.NotNull
+import java.rmi.server.ServerNotActiveException
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.math.log
+import kotlin.random.Random
+
+private const val LATENCY = 700L
+private const val RESPONSE_LENGTH = 2048
+
+fun main() {
+    // Функции можно вызывать отсюда для проверки
+    //  для ДЗ лучше использовать blockingSubscribe вместо subscribe потому что subscribe подпишется на изменения,
+    //  но изменения в большинстве случаев будут получены позже, чем выполнится функция main, поэтому в консоли ничего
+    //  не будет выведено. blockingSubscribe работает синхронно, поэтому результат будет выведен в консоль
+    //
+    //  В реальных программах нужно использовать subscribe или передавать данные от источника к источнику для
+    //  асинхронного выполнения кода.
+    //
+    //  Несмотря на то, что в некоторых заданиях фигурируют слова "синхронный" и "асинхронный" в рамках текущего ДЗ
+    //  это всего лишь имитация, реальное переключение между потоками будет рассмотрено на следующем семинаре
+    println("1) Single - т. к. возвращает либо результат либо ошибку ")
+    requestDataFromServerAsync()
+    println("2) Completable - т.к. возвращает только статус выполнения, без данных")
+    requestServerAsync()
+    println("3) Maybe - проверяет значение в бд и есть onComplete")
+    requestDataFromDbAsync<String>()
+
+//    println(emitEachSecond())
+
+//    xMap { flatMapCompletable(it) }
+//    xMap { concatMapCompletable (it) }
+//  xMap { switchMapCompletable(it) }
+    emitEachSecond()
+
+}
+
+// 1) Какой источник лучше всего подойдёт для запроса на сервер, который возвращает результат? single
+// Почему?
+// Дописать функцию
+fun requestDataFromServerAsync() /* -> ???<ByteArray> */ {
+
+    // Функция имитирует синхронный запрос на сервер, возвращающий результат
+    fun getDataFromServerSync(): ByteArray? {
+        Thread.sleep(LATENCY);
+        val success = Random.nextBoolean()
+        return if (success) Random.nextBytes(RESPONSE_LENGTH) else null
+    }
+     Single.fromCallable { getDataFromServerSync() }
+         .blockingSubscribe({ println(it) }, { println(it.message) })
+
+    /* return ??? */
+}
+
+
+// 2) Какой источник лучше всего подойдёт для запроса на сервер, который НЕ возвращает результат? completable
+// Почему?
+// Дописать функцию
+fun requestServerAsync() /* -> ??? */ {
+
+    // Функция имитирует синхронный запрос на сервер, не возвращающий результат
+    fun getDataFromServerSync() {
+        Thread.sleep(LATENCY)
+        if (Random.nextBoolean()) throw ServerNotActiveException()
+    }
+    Completable.fromAction{getDataFromServerSync()}
+        .blockingSubscribe({ println("Completed") }, { println(it.message) })
+
+
+    /* return ??? */
+}
+
+// 3) Какой источник лучше всего подойдёт для однократного асинхронного возвращения значения из базы данных? maybe
+// Почему?
+// Дописать функцию
+fun <T> requestDataFromDbAsync() /* -> ??? */ {
+
+    // Функция имитирует синхронный запрос к БД не возвращающий результата
+    fun getDataFromDbSync(): T? {
+        Thread.sleep(LATENCY); return null
+
+    }
+    Maybe.fromCallable{getDataFromDbSync()}
+        .blockingSubscribe({ println(it) },{ println(it.message) },{ println("Completed") })
+
+    /* return */
+}
+
+// 4) Примените к источнику оператор (несколько операторов), которые приведут к тому, чтобы элемент из источника
+// отправлялся раз в секунду (оригинальный источник делает это в 2 раза чаще).
+// Значения должны идти последовательно (0, 1, 2 ...)
+// Для проверки результата можно использовать .blockingSubscribe(::printer)
+fun emitEachSecond() {
+
+    // Источник
+    fun source(): Flowable<Long> = Flowable.interval(500, TimeUnit.MILLISECONDS)
+
+
+
+    // Принтер
+    fun printer(value:Long) = println("${Date()}: value = $value")
+
+
+    // code here
+    //Не получилось, ну этот вариант пропускает некоторые значения( через swichmap же надо, да? у меня не получается почему-то, было бы классно если бы вы объяснили
+   source()
+       .sample(1000,TimeUnit.MILLISECONDS)
+       .blockingSubscribe(::printer)
+
+ //   Observable.interval(1000,TimeUnit.MILLISECONDS)
+ //       .switchMap{it -> source()}
+  //      .blockingSubscribe(::printer)
+
+
+}
+
+
+
+
+// 5) Функция для изучения разницы между операторами concatMap, flatMap, switchMap
+// Нужно вызвать их последовательно и разобраться чем они отличаются
+// Документацию в IDEA можно вызвать встав на функцию (например switchMap) курсором и нажав hotkey для вашей раскладки
+// Mac: Intellij Idea -> Preferences -> Keymap -> Быстрый поиск "Quick documentation"
+// Win, Linux: File -> Settings -> Keymap -> Быстрый поиск "Quick documentation"
+//
+// конструкция в аргументах функции xMap не имеет значения для ДЗ и создана для удобства вызова функции, чтобы была
+//  возможность удобно заменять тип маппинга
+//
+// Вызов осуществлять поочерёдно из функции main
+//
+//  xMap { flatMapCompletable(it) }
+//  xMap { concatMapCompletable (it) }
+//  xMap { switchMapCompletable(it) }
+//
+fun xMap(mapper: Flowable<Int>.(internalMapper: (Int) -> Completable) -> Completable) {
+
+    fun waitOneSecond() = Completable.timer(1, TimeUnit.SECONDS)
+
+    println("${Date()}: start")
+    Flowable.fromIterable(0..20)
+        .mapper { iterableIndex ->
+
+            waitOneSecond()
+                .doOnComplete { println("${Date()}: finished operation for iterable index $iterableIndex") }
+
+        }
+        .blockingSubscribe()
+}
